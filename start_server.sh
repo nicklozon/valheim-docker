ce7da3f45bc54f8ed115e227767c6fe45d9c37bc
Nick Lozon <nicklozon@gmail.com>
2021-11-03 14:45:47 -0400

    fix multi instance running

--- start_server.sh
+++ start_server.sh
@@ -3,7 +3,7 @@ export templdpath=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
 export SteamAppId=892970
 
-echo "Updating application"
+echo "Updating application..."
 /usr/games/steamcmd +login anonymous +force_install_dir /valheim +app_update 896660 +quit
 
 echo "Starting server PRESS CTRL-C to exit"
@@ -13,7 +13,7 @@ trap 'echo "Stopping server..."; kill -2 "${child_pid}"; wait "${child_pid}"; ec
 # Tip: Make a local copy of this script to avoid it being overwritten by steam.
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
-./valheim_server.x86_64 -nographics -batchmode -name "$SERVER_NAME" -world "$WORLD_NAME" -port 2456 -password "$WORLD_PASSWORD" -public 1 -savedir /valheim &
+./valheim_server.x86_64 -nographics -batchmode -name "$SERVER_NAME" -world "$DOCKER_COMPOSE_NAME" -port 2456 -password "$WORLD_PASSWORD" -public 1 -savedir /valheim &
 child_pid="$!"
 wait "${child_pid}"
 
